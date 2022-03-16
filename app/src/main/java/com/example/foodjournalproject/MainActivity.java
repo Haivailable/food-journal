777ba172890abadeda8f060560f55ac2b50f0e44
Shabbir1065 <shabbir1065@gmail.com>
2022-03-16 19:35:32 -0400

    Recreated the front-end for the navigation bar

--- app/src/main/java/com/example/foodjournalproject/MainActivity.java
+++ app/src/main/java/com/example/foodjournalproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.activity_main);
 
         // Start of User Entry Page Code
 
