c7ce01ba5dfb637bdb917b6a3fe834778ef6a193
Shabbir1065 <shabbir1065@gmail.com>
2022-03-16 18:07:45 -0400

    added viewbinding feature in build.gradle and changed the starting screen to homepage.xml
    When i was trying to make a navigation bar at the bottom during our lab session today, I kept running into problems because the MainActivity.java file is linked to the passcode_page.xml file instead of the homepage.xml file (where the navigation bar would live).
    I'm not sure y'all will understand this cuz i don't either but ya, lil summary.

--- app/src/main/java/com/example/foodjournalproject/MainActivity.java
+++ app/src/main/java/com/example/foodjournalproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.passcode_page);
+        setContentView(R.layout.homepage);
 
         // Start of User Entry Page Code
 
