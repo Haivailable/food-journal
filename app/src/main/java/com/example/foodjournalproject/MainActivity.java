c92cbd933cc7781d58d2045937711d94895fa9c3
Haivailable <andreilopez2503@gmail.com>
2022-03-16 15:59:07 -0400

    Added Text Views and Edit Texts to the user_entry_page.xml

--- app/src/main/java/com/example/foodjournalproject/MainActivity.java
+++ app/src/main/java/com/example/foodjournalproject/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.passcode_page);
+
+        // Start of User Entry Page Code
+
+
+
+        // End of User Entry Page Code
     }
+
 }
