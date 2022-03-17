b15dfcd2a398f27235f6bfcdf1bb23ca23266149
peter-tillmann88 <tillmann@my.yorku.ca>
2022-03-16 22:43:00 -0400

    The base of the app

---
+++ app/src/main/java/com/example/foodjournalproject/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.foodjournalproject;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
