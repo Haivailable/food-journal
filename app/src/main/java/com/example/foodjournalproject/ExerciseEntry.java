aa9753369195d2571d16a9e5e150de51dddd4baf
peter-tillmann88 <tillmann@my.yorku.ca>
2022-04-02 13:58:36 -0400

    Revert "Exercise page"
    This reverts commit 87086add18d33a867d087690aa26c02c6e7a8008.

--- app/src/main/java/com/example/foodjournalproject/ExerciseEntry.java
+++ app/src/main/java/com/example/foodjournalproject/ExerciseEntry.java
@@ -2,25 +2,11 @@ package com.example.foodjournalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.ArrayList;
 
 public class ExerciseEntry extends AppCompatActivity {
-
-
-    private EditText exerciseName;
-    private EditText sets;
-    private EditText reps;
-    private EditText description;
-    private ExerciseData newExerciseData;
-    public static ArrayList<ExerciseData> exerciseData = new ArrayList<>();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +15,12 @@ public class ExerciseEntry extends AppCompatActivity {
 
         Button backNSave = (Button)  findViewById(R.id.backButton_UserEntryPage);
 
-        exerciseName = (EditText) findViewById(R.id.exMultiLineText);
-        sets = (EditText) findViewById(R.id.setsMultiLineText);
-        reps = (EditText) findViewById(R.id.repsMultiLineText);
-        description = (EditText) findViewById(R.id.descMultiLineText);
-
         backNSave.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
-            {//more code to save info
-                newExerciseData = new ExerciseData(exerciseName.getText().toString(), Integer.parseInt(String.valueOf(sets.getText())), Integer.parseInt(String.valueOf(sets.getText())), description.getText().toString());
-                exerciseData.add(newExerciseData);
-
-                startActivity(new Intent(ExerciseEntry.this, MainActivity.class));
+            {
+                //more code to save info
                 finish();
             }
         });
